89730addec7b00fde37c5c86a57b340dd1471cd3
mih-18 <132575067+mih-18@users.noreply.github.com>
2023-09-15 12:07:48 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,10 @@
+#Variáveis
+#print('Olá mundo')
+#idade = 18
+#print (idade)
+#velocidade_Maxima = 240
+#print(velocidade_Maxima)
+#frase_De_Boas_Vindas = "olá, seja bem-vindo(a)!" #String
+#print (frase_de_boas_vindas)
+posso_Entrar_Em_Contato = False #Boleanos
+print (posso_Entrar_Em_Contato)
